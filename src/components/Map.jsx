9ea0d2d85c48f7fa483216354c809f698b8955d4
rayaankhan <khan.rayaan2003@gmail.com>
2024-09-03 18:58:51 +0530

    scrollable list

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ function Map() {
   const [ancientData, setAncientData] = useState([]);
   const [location, setLocation] = useState({});
   const markersRef = useRef([]);
-  const [mapCenter, setMapCenter] = useState([23.5120, 80.3290]);
+  const [mapCenter, setMapCenter] = useState([23.512, 80.329]);
   const [mapZoom, setMapZoom] = useState(4);
 
   const toggleSidebar = () => {
@@ -48,9 +48,10 @@ function Map() {
 
   const handlePlaceClick = (index) => {
     const marker = markersRef.current[index];
-    if (marker) { // Check if marker is defined
+    if (marker) {
+      // Check if marker is defined
       const latLng = marker.getLatLng();
-      
+
       setMapCenter(latLng);
       setMapZoom(6);
 
@@ -72,7 +73,7 @@ function Map() {
   return (
     <div className="relative flex flex-col md:flex-row justify-center text-blue-400 mx-5 rounded-md md:space-x-3 md:space-y-0 h-[70vh]">
       <div
-        className={`bg-slate-200 rounded-lg md:mb-0 md:w-8/12 w-full h-full align-middle overflow-hidden z-0`}
+        className={`bg-slate-200 rounded-lg md:mb-0 md:w-9/12 w-full h-full align-middle overflow-hidden z-0`}
       >
         <MapContainer
           center={mapCenter}
@@ -83,11 +84,12 @@ function Map() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <MapController center={mapCenter} zoom={mapZoom} /> {/* This ensures the map updates */}
-
+          <MapController center={mapCenter} zoom={mapZoom} />{" "}
+          {/* This ensures the map updates */}
           {ancientData.map((place, index) => {
             const coordinates = location[place.city];
-            if (coordinates) { // Check if coordinates exist
+            if (coordinates) {
+              // Check if coordinates exist
               return (
                 <Marker
                   key={index}
@@ -107,35 +109,29 @@ function Map() {
       </div>
 
       <div
-        className={`bg-slate-200 opacity-50 rounded-lg md:w-4/12 w-4/5 md:h-full text-black ${
+        className={`bg-slate-200 opacity-60 rounded-lg md:w-3/12 w-4/5 md:h-full text-black ${
           isSidebarVisible
             ? "absolute top-0 right-0 h-full w-3/5 z-30"
             : "hidden md:block"
         } justify-center`}
       >
-        LOCATIONS LIST
-        <button
-          className=" z-30 md:hidden absolute top-1/2 -left-10 transform -translate-y-1/2 bg-gray-900 text-white p-2 rounded"
-          onClick={toggleSidebar}
-        >
-          {isSidebarVisible ? ">" : "<"}
-        </button>
-        <div className="places-list">
-          <h2>Places List</h2>
-          <ul>
+        <div className="flex justify-center text-xl font-bold py-4">
+          LOCATIONS LIST
+        </div>
+        <button className="z-30 md:hidden absolute top-1/2 -left-10 transform -translate-y-1/2 bg-gray-900 text-white p-2 rounded" onClick={toggleSidebar}>{isSidebarVisible ? ">" : "<"}</button>
+        <div className="places-list px-4 overflow-y-auto h-5/6">
+          <ul className="space-y-2">
             {ancientData.map((place, index) => (
-              <li key={index}>
-                <a
-                  href="#"
-                  onClick={() => handlePlaceClick(index)} // Open the popup on click
-                >
-                  {index+1}. {place.city}
+              <li key={index} className="text-black">
+                <a href="#" onClick={() => handlePlaceClick(index)}>
+                  {index + 1}. {place.city}
                 </a>
               </li>
             ))}
           </ul>
         </div>
       </div>
+
       {!isSidebarVisible && (
         <button
           className="md:hidden absolute top-1/2 right-0 transform -translate-y-1/2 bg-gray-900 text-white p-2 rounded"
