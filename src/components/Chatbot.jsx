96ceacfc52be06ec6678e71b39ad42392be3b01d
rayaankhan <khan.rayaan2003@gmail.com>
2024-09-05 02:22:36 +0530

    fetch chatbot url added

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -23,8 +23,7 @@ const Chatbot = () => {
     setMessage('');
 
     try {
-      // const res = await fetch('https://historic-capitals-india-btp2-1.onrender.com/chat', {
-      const res = await fetch('http://localhost:5000/chat', {
+      const res = await fetch('https://historic-capitals-india-btp2-1.onrender.com/chat', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
